8703b6bc093aa70a3d6b7ce17d8054505e94ae32
BaharAlmasiMoghaddam <baharalmasiimoghaddam@gmail.com>
2025-01-19 19:18:17 +0330

    Done-1

--- 1-todo-list/script.js
+++ 1-todo-list/script.js
@@ -1,12 +1,30 @@
 function todoList(todos) {
-  // Write your code here...
+  const content = document.getElementById("content");
+  const ul = document.createElement("ul");
+
+  todos.forEach((todoItem) => {
+    const li = document.createElement("li");
+    li.textContent = todoItem.todo;
+
+    li.addEventListener("click", () => {
+      if (li.style.textDecoration === "line-through") {
+        li.style.textDecoration = "none";
+        alert("Oops! You haven't done it yet.");
+      } else {
+        li.style.textDecoration = "line-through";
+        alert("Yay! Successfully completed.");
+      }
+    });
+    ul.appendChild(li);
+  });
+  content.appendChild(ul);
 }
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
